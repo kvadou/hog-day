98b881aa2098cae643667f442bcd3b1a2a87b429
Doug Kvamme <dougkvamme@gmail.com>
2023-01-29 06:50:42 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import pig from './pig.jpg';
 import './App.css';
 
-function App() {
+function GroundhogDayCounter() {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const groundhogDay = new Date("February 2, 2023 06:20:00").getTime();
+      const now = new Date().getTime();
+      const distance = groundhogDay - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setDays(days);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={pig} className="App-logo" alt="logo" />
+    <div>
+      <h1>Countdown to Groundhog Day:</h1>
+      <h2>{days} days {hours} hours {minutes} minutes {seconds} seconds</h2>
+    </div>
+  </header>
     </div>
   );
 }
 
-export default App;
+export default GroundhogDayCounter;
